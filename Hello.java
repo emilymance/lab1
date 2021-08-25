37104997804e1dd1d706240f1eab744b9dddcdd3
emilymance <69824503+emilymance@users.noreply.github.com>
2021-08-25 13:05:48 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/*
+
+Hello.java
+
+Emily Mance
+
+8/25/21
+
+255 004
+
+*/
+
+public class Hello {
+	public static void main (String [] args) {
+
+		System.out.println("<Emily Mance>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Intro to Javascript>");
+		System.out.println("<8/25/21>");
+		System.out.println("<255 004>");
+		System.out.println();
+
+	}
+}
